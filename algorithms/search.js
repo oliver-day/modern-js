814634067548b73b0daa35b10deef8129eb350af
Oliver Day <oliver.day.swe@gmail.com>
2023-06-25 10:38:30 -0700

    add 'algorithms/search'

---
+++ algorithms/search.js
@@ -0,0 +1,31 @@
+// Problem: Create a function that takes a sorted array and a target value. Return the index of the target value in the array. If the target value is not in the array, return -1.
+
+function search(arr, target, start = 0, end = arr.length - 1) {
+
+  console.log(start, end)
+
+  if (start > end) {
+      console.log('Not found!');
+      return -1;
+  }
+
+  const middle = Math.floor( (start + end) / 2 );
+
+  if (arr[middle] === target) {
+      console.log(`${target} Found at index ${middle}`);
+      return middle;
+  }
+
+  if(arr[middle] > target) {
+      return search(arr, target, start, middle-1);
+  }
+
+  if(arr[middle] < target) {
+      return search(arr, target, middle+1, end);
+  }
+}
+
+const arr = ['a', 'b', 'c', 'x', 'y', 'z'];
+console.log(arr);
+console.log('Target: b');
+console.log(search(arr, 'b'));
