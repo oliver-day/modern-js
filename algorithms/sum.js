aa03e89ae350042b4d47287573c2bfe975824ab1
Oliver Day <oliver.day.swe@gmail.com>
2023-06-25 10:25:19 -0700

    add 'algorithms/sum'

---
+++ algorithms/sum.js
@@ -0,0 +1,21 @@
+// Problem: Create a function that takes an array of numbers and returns a number that is the sum of all values in the array.
+
+
+
+// Solution 1
+function cumSum(arr) {
+  return arr.reduce((acc, cur) => acc + cur, 0);
+}
+
+// Solution 2 (w/o JavaScript built-in reduce method)
+// export function cumSum(arr) {
+// let total = 0;
+
+// for(let i = 0; i < arr.length; i++) {
+//   total += arr[i];
+// } 
+
+// return total;
+// }
+
+console.log('sum: ', cumSum([1,3,5,7]));
