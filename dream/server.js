9f4b2e71f9922dd61de50e496544e3769a27d9f6
Oliver Day <oliver.day.swe@gmail.com>
2023-06-25 17:55:45 -0700

    feat: add server logic

---
+++ dream/server.js
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+
+// Allows us to use environment variables in .env file
+dotenv.config();
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+import express from 'express';
+import cors from 'cors';
+
+// Configure middleware
+const app = express();
+app.use(cors());
+app.use(express.json()); // tells our API to accept JSON as body data
+
+// Define routes
+app.post('/dream', async (req, res) => {
+  const { prompt } = req.body;
+
+  const aiResponse = await openai.createImage({
+    prompt,
+    n: 1,
+    size: '1024x1024',
+  });
+
+  const image = aiResponse.data.data[0].url;
+  res.send({ image });
+});
+
+// Start server
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
